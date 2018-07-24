8744d71a12d5c933aecb83b61af3c708ce17a628
Rayhan Chowdhury <rayhan379@users.noreply.github.com>
2018-07-25 00:32:51 +0600

    Add files via upload

---
+++ serics.c
@@ -0,0 +1,12 @@
+int main()
+{
+    int i,n,sum=0;
+    printf("Enter an integer: ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        sum+=i;
+    }
+    printf("Sum: %d",sum);
+    return 0;
+}
